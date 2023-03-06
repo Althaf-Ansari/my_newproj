7bde5406f5c2cd771e0a443c3971b0f8d9b15ca9
Althaf-Ansari <althafansari67@gmail.com>
2023-03-06 16:33:40 +0530

    Updates

---
+++ precache-manifest.a3fab0e064c7ca715e2819a408f693f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b208438014a89e5cfeeb",
+    "url": "/my_newproj/static/css/main.ecc92de0.chunk.css"
+  },
+  {
+    "revision": "b208438014a89e5cfeeb",
+    "url": "/my_newproj/static/js/main.b2084380.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/my_newproj/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "5be580f21f69854f3089",
+    "url": "/my_newproj/static/css/2.c0878ff6.chunk.css"
+  },
+  {
+    "revision": "5be580f21f69854f3089",
+    "url": "/my_newproj/static/js/2.5be580f2.chunk.js"
+  },
+  {
+    "revision": "b467a825965bbb55b6c1",
+    "url": "/my_newproj/static/js/runtime~main.b467a825.js"
+  },
+  {
+    "revision": "bb98bf3d84dd75f6e6a468782d4045c5",
+    "url": "/my_newproj/index.html"
+  }
+];
